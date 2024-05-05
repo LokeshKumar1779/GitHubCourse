1ea71e4e2b50de447cbd375c05c0432aaa92fbb8
LokeshKumar1779 <kumarlokesh57@gmail.com>
2024-05-05 11:27:15 +0530

    Sign up code added

--- src/main/java/pages/SignUpPage.java
+++ src/main/java/pages/SignUpPage.java
@@ -1,18 +1,36 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SignUpPage extends Page{
+
+    @FindBy(css = "input[data-qa='signup-name']")
+    private WebElement signup_name;
+
+    @FindBy(css = "input[data-qa='signup-email']")
+    private WebElement signup_email;
+
+    @FindBy(css = "button[data-qa='signup-button']")
+    private WebElement signup_button;
 
     public SignUpPage(WebDriver driver) {
         super(driver);
     }
 
-    public SignUpPage enterPersonalDetails(String firstName, String lastName) {
+    public SignUpPage fillSignupDetails(String username, String email_address){
+        signup_name.sendKeys(username);
+        signup_email.sendKeys(email_address);
+        signup_button.click();
         return this;
     }
 
-    public SignUpPage enterAddressDetails(String addLine1, String addLine2, String city, String state, String country, String zipcode) {
+    public SignUpPage enterAccountInfo(String firstName, String lastName) {
+        return this;
+    }
+
+    public SignUpPage enterAddressInfo(String addLine1, String addLine2, String city, String state, String country, String zipcode) {
         return this;
     }
 }
