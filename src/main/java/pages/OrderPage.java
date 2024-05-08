ac788f3c0304c2a9515765275d3659cfa63ed809
LokeshKumar1779 <kumarlokesh57@gmail.com>
2024-05-08 16:16:00 +0530

    order page changes

--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -1,4 +1,12 @@
-public class OrderPage extends Page{
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class OrderPage extends Page {
 
      WebDriver driver;
     @FindBy(css = "#id")
