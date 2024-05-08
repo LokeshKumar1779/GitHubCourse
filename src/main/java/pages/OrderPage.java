fb477bb57eadc465bda67ba1d7e7b603d72ec57e
LokeshKumar1779 <kumarlokesh57@gmail.com>
2024-05-08 21:58:11 +0530

    line 6 added

--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -25,5 +25,6 @@ public class OrderPage extends Page {
         System.out.println("Adding 3 item to cart");
         System.out.println("Adding 4 item to cart");
         System.out.println("Adding 5 item to cart");
+        System.out.println("Adding 6 item to cart");
     }
 }
