84b722d70c6a976a495ced974047751aed582d03
LokeshKumar1779 <kumarlokesh57@gmail.com>
2024-05-09 19:45:24 +0530

    Cherry Page Line5

--- src/main/java/pages/CherryPage.java
+++ src/main/java/pages/CherryPage.java
@@ -11,5 +11,7 @@ public class CherryPage extends Page{
         System.out.println("line1");
         System.out.println("line2");
         System.out.println("line3");
+        System.out.println("line4");
+        System.out.println("line5");
     }
 }
