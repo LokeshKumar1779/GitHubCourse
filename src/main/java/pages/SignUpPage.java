9e094254cb68174a8c949ae0f289d18ac811b947
LokeshKumar1779 <kumarlokesh57@gmail.com>
2024-05-09 16:55:57 +0530

    Revert "Merge branch 'register'"
    This reverts commit 50b3d5361b06276f0c79751fad216bfe69a7580d, reversing
    changes made to 8e9399b9cf46f63491dd2d91107d791255a43be3.

--- src/main/java/pages/SignUpPage.java
+++ src/main/java/pages/SignUpPage.java
@@ -1,14 +1,10 @@
 package pages;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
-import org.w3c.dom.html.HTMLInputElement;
 
-public class SignUpPage extends Page {
+public class SignUpPage extends Page{
 
     @FindBy(css = "input[data-qa='signup-name']")
     private WebElement signup_name;
@@ -19,49 +15,18 @@ public class SignUpPage extends Page {
     @FindBy(css = "button[data-qa='signup-button']")
     private WebElement signup_button;
 
-    @FindBy(id = "id_gender1")
-    private WebElement genderRadioBtn1;
-    @FindBy(id = "password")
-    private WebElement passwordTextField;
-    @FindBy(id = "days")
-    private WebElement daysElement;
-    @FindBy(id = "months")
-    private WebElement months;
-    @FindBy(css = "#years")
-    private WebElement years;
-    @FindBy(css = "#newsletter")
-    private WebElement newsletter;
-    @FindBy(css = "#optin")
-    private WebElement optin;
-
     public SignUpPage(WebDriver driver) {
         super(driver);
     }
 
-    public SignUpPage fillSignupDetails(String username, String email_address) {
+    public SignUpPage fillSignupDetails(String username, String email_address){
         signup_name.sendKeys(username);
         signup_email.sendKeys(email_address);
         signup_button.click();
         return this;
     }
 
-    public SignUpPage enterAccountInfo(String title, String day, String month, String year) {
-//        Select Male gender
-        driver.findElement(By.cssSelector("input[value='" + title + "']")).click();
-        genderRadioBtn1.click();
-        passwordTextField.sendKeys("password");
-
-
-//        Select day for dob
-        selectByValue(daysElement, day);
-//        Select month for dob
-        selectByValue(months, month);
-//        Select year for dob
-        selectByValue(years, year);
-//        Newsletter checkbox
-        newsletter.click();
-//        Special offer checkbox
-        optin.click();
+    public SignUpPage enterAccountInfo(String firstName, String lastName) {
         return this;
     }
 
