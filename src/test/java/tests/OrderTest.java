8da05f4b20fcf7cb82aaeba1c3ded0fb769d2948
LokeshKumar1779 <kumarlokesh57@gmail.com>
2024-05-09 08:27:39 +0530

    Order Line3

--- src/test/java/tests/OrderTest.java
+++ src/test/java/tests/OrderTest.java
@@ -5,6 +5,7 @@ public class OrderTest extends BaseTest {
     public void addToCart(){
         System.out.println("Line 1");
         System.out.println("Line 2");
+        System.out.println("Line 3");
     }
 
   
