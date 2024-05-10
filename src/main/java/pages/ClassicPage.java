84282b1b4032964ca15e6f4edc6aca13214fb35e
LokeshKumar1779 <89783109+LokeshKumar1779@users.noreply.github.com>
2024-05-10 09:37:34 +0530

    Update 3 ClassicPage.java
    int i = 20 added

--- src/main/java/pages/ClassicPage.java
+++ src/main/java/pages/ClassicPage.java
@@ -5,5 +5,6 @@ public class ClassicPage {
     public void classicMethod1(){
         System.out.println("Hello Classic!!");
         System.out.println("Hi Classic");
+        int i = 20;
     }
 }
